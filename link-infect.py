166632a3c8872b14ac4bf62a5cd4a3e0bb3aaae7
MR-DARK15 <144409542+MR-DARK15@users.noreply.github.com>
2023-09-25 21:59:11 +0330

    Add files via upload

---
+++ link-infect.py
@@ -0,0 +1,63 @@
+from os import system
+from time import sleep
+from colorama import Fore
+import webbrowser
+system("clear")
+print("")
+  
+print(Fore.LIGHTGREEN_EX+"""
+                 ,-""-.
+                / ,--. \
+               | ( () ) |  ┌─┐┌─┐┌─┐ ┬ ┬
+                \ `--` /   └─┐├┤ ├┤  │ │
+                 `-..-`    └─┘└─┘└─┘ └─┘
+              ::: Tool By MR - DARK :::
+            ::: github.com/MR-DARK15 :::
+  
+            [1]➟ link infect
+            [2]➟ link mokharab
+            [3]➟ update-Scrypt
+            [4]➟ Telegram-ID
+            [5]➟ Telegram-chanel
+            [00]Exit
+""")
+print("")
+name = input(Fore.LIGHTGREEN_EX+"Select Option: ")
+print("")
+sleep(2)
+system("clear")
+print("")
+  
+if name == "1":
+  system("clear")
+  print(Fore.LIGHTRED_EX+"          Hi, how do you copy the link below and paste it in the filtering code🙂❤️")
+  print("")
+  print(Fore.LIGHTGREEN_EX+"         link"+Fore.LIGHTRED_EX+"-: "+Fore.LIGHTCYAN_EX+"https://tinyurl.com/mokharab-infect")
+  print("")
+  print(Fore.LIGHTYELLOW_EX+          "Script made by Mr. Dark Hacker I hope you enjoy the script God bless you👋")
+  print("")
+  exit()
+  
+if name == "2":
+  system("clear")
+  print(Fore.LIGHTRED_EX+"          Hi, how do you copy the link below and paste it in the filtering code🙂❤️")
+  print("")
+  print(Fore.LIGHTGREEN_EX+"       link"+Fore.LIGHTRED_EX+"-: "+Fore.LIGHTCYAN_EX+"https://tinyurl.com/linklib3-mokharab")
+  print("")
+  print(Fore.LIGHTYELLOW_EX+          "Script made by Mr. Dark Hacker I hope you enjoy the script God bless you👋")
+  print("")
+  exit()
+  
+if name == "3":
+  system("apt update")
+  system("apt upgrade")
+  exit()
+  
+if name == "4":
+  print(Fore.LIGHTYELLOW_EX+"Telegram"+Fore.LIGHTRED_EX+"-: "+Fore.LIGHTCYAN_EX+"https://t.me/name_Mr_dark")
+  
+if name == "5":
+  print(Fore.LIGHTYELLOW_EX+"Telegram chanell"+Fore.LIGHTRED_EX+"-: "+Fore.LIGHTCYAN_EX+"https://t.me/MR_DARK_Teem")
+  
+if name == "00":
+  exit()
